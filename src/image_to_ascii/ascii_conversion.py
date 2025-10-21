9c1ac1475f9883b6b1840c484f1169c8f2e4081c
Kimberley Bezuidenhout <kimberley.bezuidenhout@gmail.com>
2025-10-21 10:11:40 +0000

    dithering before ASCII conversion (I hope its okay.)

--- src/image_to_ascii/ascii_conversion.py
+++ src/image_to_ascii/ascii_conversion.py
@@ -1,36 +1,19 @@
 import numpy as np
 from .image_processing import resize_image
 from .symbol_selection import select_symbol
-from .dithering import apply_dithering  # make sure dithering.py is in the same package
+from .dithering import apply_dithering  
 
-
-def image_to_ascii(img, width, dithering="none"):
-    """
-    Convert a PIL image to ASCII using block-based symbol selection.
-
-    Args:
-        img: PIL.Image
-        width: int, target ASCII width
-        dithering: str, optional, one of "none", "floyd", "atkinson"
-                   applies dithering before ASCII conversion
-    Returns:
-        str: ASCII art
-    """
-    
+def image_to_ascii(img, width, dither=False):
     img = resize_image(img, width)
 
-    
+    if dither:
+        img = apply_dithering(img)
+
     pixels = np.array(img)
-
-    # Apply dithering if requested
-    if dithering != "none":
-        pixels = apply_dithering(pixels, method=dithering)
-
     ascii_height = img.height
     ascii_width = width
     block_h = img.height // ascii_height
     block_w = img.width // ascii_width
-
     ascii_img = []
 
     for y in range(ascii_height):
@@ -45,21 +28,3 @@ def image_to_ascii(img, width, dithering="none"):
         ascii_img.append(''.join(row))
 
     return '\n'.join(ascii_img)
-
-
-#### --- optional inline test ---
-if __name__ == "__main__":
-    image_to_ascii()
-    # from PIL import Image
-
-    # img = Image.open("test.jpg").convert("L")
-    # ascii_art_none = image_to_ascii(img, width=80)
-    # ascii_art_floyd = image_to_ascii(img, width=80, dithering="floyd")
-    # ascii_art_atkinson = image_to_ascii(img, width=80, dithering="atkinson")
-
-    # print("=== No Dithering ===")
-    # print(ascii_art_none)
-    # print("\n=== Floyd-Steinberg Dithering ===")
-    # print(ascii_art_floyd)
-    # print("\n=== Atkinson Dithering ===")
-    # print(ascii_art_atkinson)
