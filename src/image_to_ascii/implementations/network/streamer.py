ebda8dff0b72492b5f26b67532c2324041afc7bd
gumumjhb025 <gumumjhb025@student.wethinkcode.co.za>
2025-10-22 16:58:53 +0200

    ascii_live stream

--- src/image_to_ascii/implementations/network/streamer.py
+++ src/image_to_ascii/implementations/network/streamer.py
@@ -3,52 +3,66 @@ import pickle as pk
 import cv2 as cv
 import numpy as np
 from image_to_ascii.implementations.base_converter import convert_image_to_ascii
+from image_to_ascii.implementations.mode_converter import cvt_scale_np
+from image_to_ascii.implementations.method import Filter
+
 from PIL import Image as ImageSrc
 from PIL.Image import Image
+import asyncio
+import websockets as ws
 
 
-class Stream:
-    def __init__(self):
-        self.socket = sk.socket(sk.AF_INET,sk.SOCK_DGRAM)
-        self.initStream()
-        self.stream()
 
-    def initStream(self):
-        self.socket.bind(("0.0.0.0",8080))
-        print(":: Streaming Socket 0.0.0.0:8080")
+
+    
+
+async def initStream():
+    port=5555
+    host="20.20.23.98"
+    async with ws.serve(main,host,port):
+        await asyncio.Future()  
+    
+    
+
+def modify(image:np.ndarray)->np.ndarray:
+    image=cvt_scale_np(image,Filter.AVERAGE)
+    ascii_frame = convert_image_to_ascii(image,200)
+    return ascii_frame
+
+
+
+async def main(socket):
+    instance = cv.VideoCapture(0)
+    try:
+        while True:
+            isFrame,frame=instance.read()
+            if isFrame:
+                frame=cv.flip(frame,1)
+                frame=modify(frame)
+                await stream(frame,socket)
+            if cv.waitKey(1) and ord("q") == 0xFF:
+                break
+    except KeyboardInterrupt:
         
-    def modify(self,image:np.ndarray)->np.ndarray:
-        print(image)
-        return image
+        instance.release()
+        cleanup()
 
 
-
-    def stream(self):
-        self.instance = cv.VideoCapture(0)
-        try:
-            while True:
-                isFrame,frame=self.instance.read()
-                if isFrame:
-                    frame=cv.flip(frame,1)
-                    frame=self.modify(frame)
-                    cv.imshow("this is me",frame)
-                if cv.waitKey(1) and ord("q") == 0xFF:
-                    break
-        except KeyboardInterrupt:
-            self.cleanup()
+async def stream(frame:str,sk):
+    await sk.send(str(frame))
+    await asyncio.sleep(1/30)
+    #self.client.sendall(frame.encode())
 
 
-    def cleanup(self):
-        print("\nCleaning UP")
-        self.instance.release()
-        self.socket.close()
-        cv.destroyAllWindows()
+def cleanup():
+    print("\nCleaning UP")
+    cv.destroyAllWindows()
 
-        
+    
 
 
 if __name__ == "__main__":
-    Stream()
+    asyncio.run(initStream())
 
 
 
