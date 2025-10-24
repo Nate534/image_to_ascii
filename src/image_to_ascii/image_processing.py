c1f4e9f6350a635fd89100a3b821d6ececdbf0b9
gumumjhb025 <gumumjhb025@student.wethinkcode.co.za>
2025-10-24 10:43:07 +0200

    cleaned up on streamer file

--- src/image_to_ascii/image_processing.py
+++ src/image_to_ascii/image_processing.py
@@ -1,13 +1,13 @@
 from PIL import Image
 from .implementations.method import Filter
 
+
 def load_image(path):
-    img = Image.open(path).convert('L')
+    img = Image.open(path).convert("L")
     return img
+
 
 def resize_image(img, width):
     aspect = img.height / img.width
     height = int(width * aspect / 2)
     return img.resize((width, height))
-
-
--- src/image_to_ascii/implementations/mode_converter.py
+++ src/image_to_ascii/implementations/mode_converter.py
@@ -4,14 +4,10 @@ from .method import Filter
 import numpy as np
 
 
-
-def cvt_scale_np(img:np.ndarray,mode:Filter)->Image:
-    
-   
+def cvt_scale_np(img: np.ndarray, mode: Filter) -> Image:
     arr = img
 
-    
-    gray = 0.299 * arr[:,:,0] + 0.587 * arr[:,:,1] + 0.114 * arr[:,:,2]
+    gray = 0.299 * arr[:, :, 0] + 0.587 * arr[:, :, 1] + 0.114 * arr[:, :, 2]
 
     if mode == Filter.AVERAGE:
         gray = arr.mean(axis=2)
@@ -22,13 +18,11 @@ def cvt_scale_np(img:np.ndarray,mode:Filter)->Image:
     return ImageF.fromarray(gray, mode="L")
 
 
-def cvt_scale(img:Image,mode:Filter)->Image:
-    
+def cvt_scale(img: Image, mode: Filter) -> Image:
     img = img.convert("RGB")
     arr = np.asarray(img, dtype=float)
 
-    
-    gray = 0.299 * arr[:,:,0] + 0.587 * arr[:,:,1] + 0.114 * arr[:,:,2]
+    gray = 0.299 * arr[:, :, 0] + 0.587 * arr[:, :, 1] + 0.114 * arr[:, :, 2]
 
     if mode == Filter.AVERAGE:
         gray = arr.mean(axis=2)
